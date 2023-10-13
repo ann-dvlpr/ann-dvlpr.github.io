e1190a475f34046318d3a44d8b200c7a0dffdb2e
Fluid.dev <142986025+fluid-developer@users.noreply.github.com>
2023-10-13 21:55:07 +0300

    Update redirects.js

--- redirects.js
+++ redirects.js
@@ -1,6 +1,10 @@
 // about me
-document.getElementById("aboutme").addEventListener("click", aboutme);
-function redirect(){ window.location = "https://fluid-developer.github.io/aboutme.html"; }
+function aboutme(){
+  document.getElementById("aboutme").addEventListener("click", aboutme);
+  function redirect(){ window.location = "https://fluid-developer.github.io/aboutme.html"; }
+  {
 // subsite test
-document.getElementById("subsitetest").addEventListener("click", subsitetest);
-function redirect(){ window.location = "https://fluid-developer.github.io/subsite.html"; }
+function subsitetest(){
+  document.getElementById("subsitetest").addEventListener("click", subsitetest);
+  function redirect(){ window.location = "https://fluid-developer.github.io/subsite.html"; }
+  {
