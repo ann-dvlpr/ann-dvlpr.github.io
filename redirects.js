4ac30fffdcdb35654532ac5101106d5926dd7c94
Fluid.dev <142986025+fluid-developer@users.noreply.github.com>
2023-10-13 21:35:44 +0300

    Create redirects.js

---
+++ redirects.js
@@ -0,0 +1,6 @@
+// about me
+document.getElementById("aboutme").addEventListener("click", aboutme);
+function redirect(){ window.location = "https://fluid-developer.github.io/aboutme.html"; }
+// subsite test
+document.getElementById("subsitetest").addEventListener("click", subsitetest);
+function redirect(){ window.location = "https://fluid-developer.github.io/subsite.html"; }
